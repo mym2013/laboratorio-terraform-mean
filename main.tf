6a2b65597dccfb8e16c8c9d4add6e32613b59fd0
Gonzalo <torresdelfierro@gmail.com>
2026-01-31 11:48:19 -0300

    fix: corrige rutas de módulos y deja init funcional

--- main.tf
+++ main.tf
@@ -12,14 +12,14 @@ provider "aws" {
 }
 
 module "network" {
-  source = "./modules/vpc"
+  source = "./modules/network"
 
   project_name = var.project_name
   environment  = var.environment
 }
 
 module "security_groups" {
-  source = "./modules/security_group"
+  source = "./modules/security_groups"
 
   project_name = var.project_name
   environment  = var.environment
@@ -27,14 +27,24 @@ module "security_groups" {
   my_ip        = var.my_ip
 }
 
-module "ec2" {
-  source = "./modules/ec2"
+module "ec2_app" {
+  source = "./modules/ec2_app"
 
-  project_name      = var.project_name
-  environment       = var.environment
-  subnet_id         = module.network.public_subnets[0]
-  security_group_id = module.security_groups.ec2_sg_id
-  key_name          = var.key_name
+  project_name = var.project_name
+  environment  = var.environment
+  subnet_id    = module.network.public_subnets[0]
+  sg_id        = module.security_groups.ec2_sg_id
+  key_name     = var.key_name
+}
+
+module "ec2_mongo" {
+  source = "./modules/ec2_mongo"
+
+  project_name = var.project_name
+  environment  = var.environment
+  subnet_id    = module.network.public_subnets[1]
+  sg_id        = module.security_groups.ec2_sg_id
+  key_name     = var.key_name
 }
 
 module "alb" {
@@ -46,4 +56,3 @@ module "alb" {
   public_subnets        = module.network.public_subnets
   alb_security_group_id = module.security_groups.alb_sg_id
 }
-
