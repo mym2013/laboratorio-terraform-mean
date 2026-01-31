6c643397ae392b5771bc581abb8a73d11af375be
Gonzalo <torresdelfierro@gmail.com>
2026-01-31 11:49:53 -0300

    fix: ajuste en modulo alb

---
+++ modules/alb/main.tf
@@ -0,0 +1,45 @@
+resource "aws_lb" "this" {
+  name               = "${var.project_name}-${var.environment}-alb"
+  load_balancer_type = "application"
+  internal           = false
+  security_groups    = [var.alb_security_group_id]
+  subnets            = var.public_subnets
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-alb"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_target_group" "this" {
+  name     = "${var.project_name}-${var.environment}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name        = "${var.project_name}-${var.environment}-tg"
+    Environment = var.environment
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
+}
